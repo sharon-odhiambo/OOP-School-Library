67c231904eb1c1fb39a6152454a2085eade2c3a7
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-10-05 13:07:48 +0300

    implement correct-name in Nameable and turn Person into Nameable

--- person.rb
+++ person.rb
@@ -1,4 +1,8 @@
-class Person
+require_relative './basedecorator.rb'
+require_relative './capitalizedecorator.rb'
+require_relative './trimmerdecorator.rb'
+
+class Person < Nameable
   attr_writer :id
   attr_accessor :name, :age
 
@@ -20,4 +24,16 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+p  capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+p  capitalizedTrimmedPerson.correct_name
+
