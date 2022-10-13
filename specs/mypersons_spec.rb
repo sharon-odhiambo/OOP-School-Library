b26b861eb2214137370ced7088acdb826dabb641
businesskaniki <businesskanikii@gmail.com>
2022-10-13 11:16:04 +0300

    added test for rental class

--- specs/mypersons_spec.rb
+++ specs/mypersons_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe Person do 
     it "takes no parameter and returns a person object" do
-        @person = Person.new
+        @person = Person.new(20,'name')
         @person.should be_an_instance_of Person
     end
 end
